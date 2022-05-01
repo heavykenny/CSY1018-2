9cc8f68bea07f253713bf9ca787618dbedf1faa5
Kehinde Olawuwo <olawuwokenny@gmail.com>
2022-05-01 20:14:04 +0100

    Updating code to use mordern js let and equality

--- game.js
+++ game.js
@@ -1,102 +1,104 @@
-var upPressed = false;
-var downPressed = false;
-var leftPressed = false;
-var rightPressed = false;
-var lastPressed = false;
+let upPressed = false;
+let downPressed = false;
+let leftPressed = false;
+let rightPressed = false;
+let lastPressed = false;
+
+let startGame;
+let body;
+let isGamePlaying = false;
 
 function keyup(event) {
-	var player = document.getElementById('player');
-	if (event.keyCode == 37) {
-		leftPressed = false;
-		lastPressed = 'left';
-	}
-	if (event.keyCode == 39) {
-		rightPressed = false;
-		lastPressed = 'right';
-	}
-	if (event.keyCode == 38) {
-		upPressed = false;
-		lastPressed = 'up';
-	}
-	if (event.keyCode == 40) {
-		downPressed = false;
-		lastPressed = 'down';
-	}
+    let player = document.getElementById('player');
+    if (event.keyCode === 37) {
+        leftPressed = false;
+        lastPressed = 'left';
+    }
+    if (event.keyCode === 39) {
+        rightPressed = false;
+        lastPressed = 'right';
+    }
+    if (event.keyCode === 38) {
+        upPressed = false;
+        lastPressed = 'up';
+    }
+    if (event.keyCode === 40) {
+        downPressed = false;
+        lastPressed = 'down';
+    }
 
-	player.className = 'character stand ' + lastPressed;
+    player.className = 'character stand ' + lastPressed;
 }
-
 
 function move() {
-	var player = document.getElementById('player');
-	var positionLeft = player.offsetLeft;
-	var positionTop = player.offsetTop;
-	if (downPressed) {
-		var newTop = positionTop+1;
+    let player = document.getElementById('player');
+    let positionLeft = player.offsetLeft;
+    let positionTop = player.offsetTop;
+    if (downPressed) {
+        let newTop = positionTop + 1;
 
-		var element = document.elementFromPoint(player.offsetLeft, newTop+32);
-		if (element.classList.contains('sky') == false) {
-			player.style.top = newTop + 'px';	
-		}
+        let element = document.elementFromPoint(player.offsetLeft, newTop + 32);
+        if (element.classList.contains('sky') === false) {
+            player.style.top = newTop + 'px';
+        }
 
-		if (leftPressed == false) {
-			if (rightPressed == false) {
-				player.className = 'character walk down';
-			}
-		}
-	}
-	if (upPressed) {
-		var newTop = positionTop-1;
+        if (leftPressed === false) {
+            if (rightPressed === false) {
+                player.className = 'character walk down';
+            }
+        }
+    }
+    if (upPressed) {
+        let newTop = positionTop - 1;
 
-		var element = document.elementFromPoint(player.offsetLeft, newTop);
-		if (element.classList.contains('sky') == false) {
-			player.style.top = newTop + 'px';	
-		}
-		
-		if (leftPressed == false) {
-			if (rightPressed == false) {
-				player.className = 'character walk up';
-			}
-		}
-	}
-	if (leftPressed) {
-		var newLeft = positionLeft-1;
+        let element = document.elementFromPoint(player.offsetLeft, newTop);
+        if (element.classList.contains('sky') === false) {
+            player.style.top = newTop + 'px';
+        }
 
-		var element = document.elementFromPoint(newLeft, player.offsetTop);
-		if (element.classList.contains('sky') == false) {
-			player.style.left = newLeft + 'px';	
-		}
+        if (leftPressed === false) {
+            if (rightPressed === false) {
+                player.className = 'character walk up';
+            }
+        }
+    }
+    if (leftPressed) {
+        let newLeft = positionLeft - 1;
+
+        let element = document.elementFromPoint(newLeft, player.offsetTop);
+        if (element.classList.contains('sky') === false) {
+            player.style.left = newLeft + 'px';
+        }
 
 
-		player.className = 'character walk left';
-	}
-	if (rightPressed) {
-		var newLeft = positionLeft+1;
-		
-		var element = document.elementFromPoint(newLeft+32, player.offsetTop);
-		if (element.classList.contains('sky') == false) {
-			player.style.left = newLeft + 'px';		
-		}
+        player.className = 'character walk left';
+    }
+    if (rightPressed) {
+        let newLeft = positionLeft + 1;
 
-		player.className = 'character walk right';
-	}
+        let element = document.elementFromPoint(newLeft + 32, player.offsetTop);
+        if (element.classList.contains('sky') === false) {
+            player.style.left = newLeft + 'px';
+        }
+
+        player.className = 'character walk right';
+    }
 
 }
 
-
 function keydown(event) {
-	if (event.keyCode == 37) {
-		leftPressed = true;
-	}
-	if (event.keyCode == 39) {
-		rightPressed = true;
-	}
-	if (event.keyCode == 38) {
-		upPressed = true;
-	}
-	if (event.keyCode == 40) {
-		downPressed = true;
-	}
+    if (event.keyCode === 37) {
+        leftPressed = true;
+    }
+    if (event.keyCode === 39) {
+        rightPressed = true;
+    }
+    if (event.keyCode === 38) {
+        upPressed = true;
+    }
+    if (event.keyCode === 40) {
+        downPressed = true;
+    }
 }
 
 
